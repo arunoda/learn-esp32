9c6e88ed65e5398ed35b702731a763fb960c48e3
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-08-31 23:52:32 +0530

    Introduce a task for handling the button

--- 04-button-inturrupts/main/main.c
+++ 04-button-inturrupts/main/main.c
@@ -3,6 +3,7 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h"
+#include "esp_log.h"
 
 #define LED_PIN GPIO_NUM_35 
 #define BUTTON_PIN GPIO_NUM_17
@@ -31,6 +32,22 @@ static void IRAM_ATTR button_isr(void *arg)
     }
 }
 
+static void button_task(void *arg)
+{
+    for (;;) {
+        button_event_t event;
+        // We get this event when the button state changed.
+        // Also, we get this event multiple times too.
+        // To change the state, this is the best way to do even if we set it multiple times.
+        // We can use a 100nf capacitor from the pin to ground to debounce press side via hardware
+        if (xQueueReceive(button_queue, &event, portMAX_DELAY) == pdTRUE) {
+            bool button_pressed = gpio_get_level(BUTTON_PIN) == 0;
+            ESP_LOGI("button", "Button changed to: %d\n", button_pressed);
+            gpio_set_level(LED_PIN, button_pressed);
+        }
+    }
+}
+
 void app_main(void)
 {
     // LED config
@@ -53,26 +70,19 @@ void app_main(void)
     };
     gpio_config(&button_conf);
 
-    // Button ISR
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(BUTTON_PIN, button_isr, (void *)(intptr_t)BUTTON_PIN);
-
     // Initiate Queue
     button_queue = xQueueCreate(10, sizeof(button_event_t));
+
+     // Button ISR
+     gpio_install_isr_service(0);
+     gpio_isr_handler_add(BUTTON_PIN, button_isr, (void *)(intptr_t)BUTTON_PIN);
 
     // Set the initial LED state
     gpio_set_level(LED_PIN, 0);
 
-    while (1) {
-        button_event_t event;
-        // We get this event when the button state changed.
-        // Also, we get this event multiple times too.
-        // To change the state, this is the best way to do even if we set it multiple times.
-        // We can use a 100nf capacitor from the pin to ground to debounce press side via hardware
-        if (xQueueReceive(button_queue, &event, portMAX_DELAY) == pdTRUE) {
-            bool button_pressed = gpio_get_level(BUTTON_PIN) == 0;
-            printf("Button changed to: %d\n", button_pressed);
-            gpio_set_level(LED_PIN, button_pressed);
-        }
-    }
+    // Run button logic on it's own task
+    // For this logic, we need at-least 3072 stack depth, otherwise we are getting stack overflow issues.
+    const UBaseType_t button_task_prio = tskIDLE_PRIORITY;
+    xTaskCreate(button_task, "button_task", 3072, NULL, button_task_prio, NULL);
+
 }
