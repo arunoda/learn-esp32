680fe8359a6036a471c5517229a6a09e23927cdf
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-09-01 23:36:35 +0530

    Add pwm example

---
+++ 05-pwm/main/main.c
@@ -0,0 +1,53 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/projdefs.h"
+#include "freertos/task.h"
+#include "driver/ledc.h"
+#include "hal/ledc_types.h"
+
+#define LED_PIN GPIO_NUM_35
+#define LED_PWM_RES 12
+#define LED_PWM_FREQ 1000
+#define LED_PWM_MAX ((1 << LED_PWM_RES) - 1)
+
+void app_main(void)
+{
+    ledc_timer_config_t timer_conf = {
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .duty_resolution = LED_PWM_RES,
+        .timer_num = LEDC_TIMER_0,
+        .freq_hz = LED_PWM_FREQ,
+        .clk_cfg = LEDC_AUTO_CLK
+    };
+    ledc_timer_config(&timer_conf);
+
+    ledc_channel_config_t channel_conf = {
+        .gpio_num = LED_PIN,
+        .speed_mode = LEDC_LOW_SPEED_MODE,
+        .channel = LEDC_CHANNEL_0,
+        .timer_sel = LEDC_TIMER_0,
+        .duty = 200,
+        .hpoint = 0
+    };
+    ledc_channel_config(&channel_conf);
+
+    const int duty_steps = 100;
+    const int delay_ms = 10;
+
+    while (1) {
+        for (int i = 0; i <= duty_steps; i++) {
+            int duty = (LED_PWM_MAX / duty_steps) * i;
+            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+            vTaskDelay(pdMS_TO_TICKS(delay_ms));
+        }
+
+        for (int i=duty_steps; i >= 0; i--) {
+            int duty = (LED_PWM_MAX / duty_steps) * i;
+            ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty);
+            ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0);
+            vTaskDelay(pdMS_TO_TICKS(delay_ms));
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(400));
+    }
+}
