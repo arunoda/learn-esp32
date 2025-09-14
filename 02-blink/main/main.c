38cca18a2e191cd0ac977250be8c7f5aa483620e
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-09-15 05:06:49 +0530

    Update the blink program

--- 02-blink/main/main.c
+++ 02-blink/main/main.c
@@ -1,28 +1,28 @@
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
+#include "hal/gpio_types.h"
+#include <stdio.h>
 
-#define LED_PIN GPIO_NUM_17
-#define LED_PIN_2 GPIO_NUM_35
+#define LED_PIN GPIO_NUM_35
 
-void app_main(void)
-{
+void app_main(void) {
+    // Set io config for the LED_PIN
+    // We need to initialize all the values, unless they will contain random values
     gpio_config_t io_conf = {
-        .pin_bit_mask = (1ULL << LED_PIN) | (1ULL << LED_PIN_2),
+        .pin_bit_mask = (1ULL << LED_PIN),
         .mode = GPIO_MODE_OUTPUT,
         .pull_up_en = GPIO_PULLUP_DISABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
         .intr_type = GPIO_INTR_DISABLE
     };
     gpio_config(&io_conf);
-
+    
     while(1) {
         gpio_set_level(LED_PIN, 1);
-        gpio_set_level(LED_PIN_2, 1);
-        vTaskDelay(200 / portTICK_PERIOD_MS);
+        vTaskDelay(pdMS_TO_TICKS(500));
 
         gpio_set_level(LED_PIN, 0);
-        gpio_set_level(LED_PIN_2, 0);
-        vTaskDelay(200 / portTICK_PERIOD_MS);
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
