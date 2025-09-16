017163e49fd7327feccbee5c7c87b95cd909ae68
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-09-16 08:48:26 +0530

    Update the button inturrupts code

--- 04-button-inturrupts/main/main.c
+++ 04-button-inturrupts/main/main.c
@@ -1,86 +1,93 @@
-#include "esp_attr.h"
+/*
+## Tasks
+* [x] app_main with led_on
+* [x] app_main with button led
+* [x] move that to a task
+* [x] use a queue to trigger the task (with a timer)
+* [ ] use ISR to trigger that queue
+*/
+
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
-#include "driver/gpio.h"
 #include "esp_log.h"
 
-#define LED_PIN GPIO_NUM_35 
+#define LED_PIN GPIO_NUM_35
 #define BUTTON_PIN GPIO_NUM_17
+#define BUTTON_TAG "button_check"
+
+QueueHandle_t button_queue;
 
 typedef struct {
     int gpio_num;
 } button_event_t;
 
-static QueueHandle_t button_queue;
-
-static void IRAM_ATTR button_isr(void *arg)
-{
+void task_button_check(void* args) {
     button_event_t event;
-    event.gpio_num = (int)(intptr_t)arg;
+    bool prev_state = false;
 
-    BaseType_t higher_task_woken = pdFALSE;
-    xQueueSendFromISR(button_queue, &event, &higher_task_woken);
-
-    // When we send an event to the queue, a higher priority task might get triggered
-    // Then we need to yeild the CPU to do that by doing a context switch
-    // This is it.
-    if (higher_task_woken == pdTRUE) {
-        portYIELD_FROM_ISR();
-    }
-}
-
-static void button_task(void *arg)
-{
-    for (;;) {
-        button_event_t event;
-        // We get this event when the button state changed.
-        // Also, we get this event multiple times too.
-        // To change the state, this is the best way to do even if we set it multiple times.
-        // We can use a 100nf capacitor from the pin to ground to debounce press side via hardware
+    while(1) {
         if (xQueueReceive(button_queue, &event, portMAX_DELAY) == pdTRUE) {
             bool button_pressed = gpio_get_level(BUTTON_PIN) == 0;
-            ESP_LOGI("button", "Button changed to: %d\n", button_pressed);
-            gpio_set_level(LED_PIN, button_pressed);
+            if (prev_state != button_pressed) {
+                ESP_LOGI(BUTTON_TAG, "Button(%d) state changed: %d", event.gpio_num, button_pressed);
+                gpio_set_level(LED_PIN, button_pressed? 1 : 0);
+            }
+            prev_state = button_pressed;
         }
     }
 }
 
-void app_main(void)
-{
-    // LED config
-    gpio_config_t io_conf = {
+static void IRAM_ATTR gpio_ist_handler(void* args) {
+    button_event_t event;
+    event.gpio_num = (int)(intptr_t)args;
+    BaseType_t higher_task = pdFALSE;
+    xQueueSendFromISR(button_queue, &event, &higher_task);
+    if (higher_task == pdTRUE) {
+        portYIELD_FROM_ISR();
+    }
+}
+
+void app_main(void) {
+    gpio_config_t led_conf = {
         .pin_bit_mask = (1ULL << LED_PIN),
         .mode = GPIO_MODE_OUTPUT,
         .pull_up_en = GPIO_PULLUP_DISABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE
     };
-    gpio_config(&io_conf);
+    gpio_config(&led_conf);
 
-    // Button config
-    gpio_config_t button_conf = {
+    gpio_config_t button_config = {
         .pin_bit_mask = (1ULL << BUTTON_PIN),
         .mode = GPIO_MODE_INPUT,
         .pull_up_en = GPIO_PULLUP_ENABLE,
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
-        .intr_type = GPIO_INTR_NEGEDGE,
+        // This may trigger multiple times when pressing the button
+        // To get a much better result, use GPIO_INTR_NEGEDGE and
+        // attach a 100nf capacitor to the pin & the ground
+        // It will give us much better accuracy but the POSEDGE is fine for our purpose.
+        .intr_type = GPIO_INTR_POSEDGE
     };
-    gpio_config(&button_conf);
+    gpio_config(&button_config);
+    gpio_install_isr_service(0);
+    // This is a nice hack: (void*)(intptr_t) BUTTON_PIN
+    // The ideal way is to pass a pointer as args
+    // But since it's just a number, we cast the number into an int pointer and then into (void*)
+    // So, we are basically smuggling a integer inside a pointer
+    // To get it back, we can do this: (int)(intptr_r)args
+    gpio_isr_handler_add(BUTTON_PIN, gpio_ist_handler, (void*)(intptr_t) BUTTON_PIN);
 
-    // Initiate Queue
     button_queue = xQueueCreate(10, sizeof(button_event_t));
 
-     // Button ISR
-     gpio_install_isr_service(0);
-     gpio_isr_handler_add(BUTTON_PIN, button_isr, (void *)(intptr_t)BUTTON_PIN);
-
-    // Set the initial LED state
-    gpio_set_level(LED_PIN, 0);
-
-    // Run button logic on it's own task
-    // For this logic, we need at-least 3072 stack depth, otherwise we are getting stack overflow issues.
-    const UBaseType_t button_task_prio = tskIDLE_PRIORITY;
-    xTaskCreate(button_task, "button_task", 3072, NULL, button_task_prio, NULL);
-
+    xTaskCreate(
+        task_button_check,
+        "button_check",
+        // If we are not doing any debug printing inside the task, we can just use 1024 as the stack depth
+        3072,
+        NULL,
+        5,
+        NULL
+    );
 }
