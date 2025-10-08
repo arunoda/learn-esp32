d2381bb4959b5c2fd4f7b23b3052c03f19e78ff0
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-10-08 10:09:37 +0530

    Add pwm support

---
+++ 32bit/04-pwm/main/main.c
@@ -0,0 +1,48 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/ledc.h"
+
+#define LED_PIN GPIO_NUM_18
+#define DUTY_RES 12
+#define NUM_TIMER LEDC_TIMER_0
+#define PWM_SPEED_MODE LEDC_LOW_SPEED_MODE
+
+void app_main(void)
+{
+    // first we need to configure the timer (timer 0)
+    ledc_timer_config_t timer_conf = {
+        .timer_num = NUM_TIMER,
+        .freq_hz = 1000,
+        .duty_resolution = DUTY_RES, // this is the resolution is in bits
+        .speed_mode = PWM_SPEED_MODE,
+        .clk_cfg = LEDC_AUTO_CLK
+    };
+    ledc_timer_config(&timer_conf);
+
+    // Then we need to assign the LED into the timer via a channel
+    ledc_channel_config_t channel_conf = {
+        .gpio_num = LED_PIN,
+        .channel = LEDC_CHANNEL_0,
+        .timer_sel = NUM_TIMER,
+        .speed_mode = PWM_SPEED_MODE,
+        .intr_type = LEDC_INTR_DISABLE,
+        .hpoint = 0,
+        .duty = 0
+    };
+    ledc_channel_config(&channel_conf);
+
+    const int num_iterations = 20;
+    const int duty_step = (1 << DUTY_RES) / num_iterations;
+
+    while(1) {
+        for (int lc = 0; lc<num_iterations; lc++) {
+            int duty = duty_step * lc;
+
+            // This is how we set the duty
+            ledc_set_duty(PWM_SPEED_MODE, LEDC_CHANNEL_0, duty);
+            ledc_update_duty(PWM_SPEED_MODE, LEDC_CHANNEL_0);
+
+            vTaskDelay(pdMS_TO_TICKS(100));
+        }
+    }
+}
